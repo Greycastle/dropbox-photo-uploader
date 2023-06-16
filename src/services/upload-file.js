82c0a8b3daf317f9cba21624af0eec2147ac286a
David Dikman <ddikman@gmail.com>
2023-06-16 14:49:52 +0900

    Swithched from REST calls to using dropbox sdk

--- src/services/upload-file.js
+++ src/services/upload-file.js
@@ -1,3 +1,4 @@
+import { Dropbox } from 'dropbox'
 import { getToken } from '@/state/auth-token'
 
 async function convertDataUrlToOctetStream(dataUrl) {
@@ -7,27 +8,12 @@ async function convertDataUrlToOctetStream(dataUrl) {
 
 export default async function uploadFile(path, dataUrl) {
   const data = await convertDataUrlToOctetStream(dataUrl)
-  const response = await fetch('https://content.dropboxapi.com/2/files/upload', {
-    headers: {
-      'authorization': `Bearer ${getToken()}`,
-      'Content-Type': 'application/octet-stream',
-      'Dropbox-API-Arg': JSON.stringify({ path, autorename: false })
-    },
-    method: 'POST',
-    body: data
-  })
 
-  if (response.status === 200) {
-    return
+  try {
+    const client = new Dropbox({ accessToken: getToken() })
+    await client.filesUpload({ path, contents: data, autorename: false })
+  } catch (err) {
+    console.log(`Failed to upload file ${err.error.error_summary}`)
+    throw new Error(`Failed to upload file ${err.error.error_summary}`)
   }
-  if (response.status === 409) {
-    console.log('File already exists: ' + path)
-    return
-  }
-  if (response.status === 401) {
-    throw new Error('Unauthorized')
-  }
-  const responseJson = await response.json()
-  console.error(`Failed to upload: ${path}`, responseJson)
-  throw new Error(`Failed to upload: ${path}`);
 }
