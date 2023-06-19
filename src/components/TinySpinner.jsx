b4a7fffac1eb27698ae6f74c108f72e2a3b3b55d
David Dikman <ddikman@gmail.com>
2023-06-19 16:21:51 +0900

    Added per file upload progress and error

---
+++ src/components/TinySpinner.jsx
@@ -0,0 +1,5 @@
+import styles from './TinySpinner.module.css'
+
+export default function TinySpinner() {
+  return <div className={styles.loader}>Loading...</div>;
+}
--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,12 +1,14 @@
 import { useState, useCallback, useContext, useMemo } from 'react'
 import capitalize from '@/services/capitalize'
-import uploadFiles from '@/services/upload-files'
-import createFolder from '@/services/create-folder'
+// import uploadFiles from '@/services/upload-files'
+// import createFolder from '@/services/create-folder'
+import startUpload from '@/services/start-upload'
 import FormContext from '@/state/form-context'
 import { logout } from '@/state/auth'
 
 import ImageCard from '@/components/ImageCard'
 import FileInput from '@/components/FileInput'
+import UploadProgress from '@/components/UploadProgress'
 
 import styles from './UploadPage.module.css'
 
@@ -15,12 +17,13 @@ function trimInput(input) {
 }
 
 export default function UploadPage() {
-  const [ images, setImages ] = useState([])
-  const [ uploadState, setUploadState ] = useState('pending')
-  const [ errorInformation, setErrorInformation ] = useState(null)
+  const [images, setImages] = useState([])
+  const [uploadState, setUploadState] = useState('pending')
+  const [errorInformation, setErrorInformation] = useState(null)
+  const [uploadTasks, setUploadTasks] = useState([])
 
   const onRemove = useCallback((index) => {
-    const updated = [ ...images ]
+    const updated = [...images]
     updated.splice(index, 1)
     setImages(updated)
   }, [images])
@@ -32,29 +35,30 @@ export default function UploadPage() {
     const mainFieldsFilled = formState.date && isSet(formState.firstName) && isSet(formState.lastName)
     const allImagesHavePurpose = images.length > 0 && images.every((_, index) => isSet(formState.imagePurpose[index]))
     return mainFieldsFilled && allImagesHavePurpose
-  }, [ formState, images ])
+  }, [formState, images])
 
   const upload = useCallback(async () => {
+    setUploadTasks([])
     setUploadState('uploading')
     try {
-      const folderName = `${capitalize(formState.firstName)}${capitalize(formState.lastName)}`
-      await createFolder(folderName)
-      await uploadFiles(folderName, images, formState)
+      const tasks = await startUpload(images, formState)
+      setUploadTasks(tasks)
+      await Promise.all(tasks.map((task) => task.promise))
+      setUploadState('success')
       setImages([])
       formState.reset()
-      setUploadState('success')
     } catch (err) {
       setErrorInformation(err.toString())
       setUploadState('failed')
       console.error('Failed to upload', err);
     }
-  }, [ formState, images ])
+  }, [formState, images])
 
   const addImages = useCallback((newImages) => {
-    setImages([ ...images, ...newImages ])
-  }, [ images ])
+    setImages([...images, ...newImages])
+  }, [images])
 
-  return <div className="w-100" style={ { 'maxWidth': '920px' } }>
+  return <div className="w-100" style={{ 'maxWidth': '920px' }}>
     <header className="d-flex justify-content-between mb-4">
       <span className="title">IxPhotoUploader</span>
       <div className="d-flex flex-row column-gap-4">
@@ -62,9 +66,9 @@ export default function UploadPage() {
         <a href="#" onClick={() => logout()}>Logout</a>
       </div>
     </header>
-    { uploadState === 'pending' && <div>
+    {uploadState === 'pending' && <div>
       <form className="d-flex flex-column row-gap-4">
-        <div className={ styles['form-inputs'] }>
+        <div className={styles['form-inputs']}>
           <label className="flex-grow-1">
             <span>Date</span>
             <input className="w-100" type="date" value={formState.date} onChange={(e) => formState.setDate(e.target.value)} />
@@ -80,26 +84,30 @@ export default function UploadPage() {
         </div>
         <FileInput onImagesAdded={addImages} />
         <div id="image-previews" className="d-flex flex-column row-gap-4">
-          { images.map((src, index) => <ImageCard key={index} index={index} src={src} onRemove={onRemove} />) }
+          {images.map((src, index) => <ImageCard key={index} index={index} src={src} onRemove={onRemove} />)}
         </div>
         <div className="d-flex flex-column gap-2 align-items-start">
           <button disabled={!isFormReady} className={styles['upload-button']} onClick={(e) => { e.preventDefault(); upload(); }}>Save photos</button>
-          { isFormReady == false && <span className='text-danger'>* Please enter all fields before upload</span> }
+          {isFormReady == false && <span className='text-danger'>* Please enter all fields before upload</span>}
         </div>
       </form>
-    </div> }
-    { uploadState === 'uploading' && <div>Uploading..</div>}
-    { uploadState === 'success' && <div>
-      <p>Upload completed!</p>
-      <button onClick={() => setUploadState('pending')}>Continue</button>
-    </div>
-    }
-    { uploadState === 'failed' && <div className="d-flex flex-column align-items-start">
-        <p>The upload failed and we cannot say for sure why. Please send this information to the developer or admin:</p>
-        <div className="mb-4">
-          <code>{ errorInformation }</code>
-        </div>
-        <button onClick={() => setUploadState('pending')}>Try again</button>
-      </div> }
+    </div>}
+    {(uploadState === 'uploading' || uploadState === 'success') && <div>
+      { uploadState === 'uploading' && <p>Uploading...</p> }
+      <UploadProgress uploadTasks={uploadTasks} />
+      {uploadState === 'success' && <div>
+        <p>Upload completed!</p>
+        <button onClick={() => setUploadState('pending')}>Continue</button>
+      </div>
+      }
+    </div>}
+    {uploadState === 'failed' && <div className="d-flex flex-column align-items-start">
+      <p>The upload failed and we cannot say for sure why. Please send this information to the developer or admin:</p>
+      <UploadProgress uploadTasks={uploadTasks} />
+      <div className="mb-4">
+        <code>{errorInformation}</code>
+      </div>
+      <button onClick={() => setUploadState('pending')}>Try again</button>
+    </div>}
   </div>
 }
